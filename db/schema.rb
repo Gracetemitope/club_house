cea7a359cc4b44d63de9a9134093ffb51f94f538
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-04-19 22:15:45 -0700

    set up bulmer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_015043) do
+ActiveRecord::Schema.define(version: 2021_04_20_020342) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "post"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
