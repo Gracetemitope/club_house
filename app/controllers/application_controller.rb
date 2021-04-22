023409ee83948a4dda153f73264605e50264150c
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-04-21 23:15:58 -0700

    fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:display_name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[display_name email])
   end
 end
